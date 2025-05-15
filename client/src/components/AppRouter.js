f840d930105e3f236d3a67741c5ca77930269dee
ZERPANTIN <zerpantin@vk.com>
2025-05-16 01:18:15 +0300

    start frontend

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {Routes, Route, Redirect } from 'react-router-dom';
+import {Switch, Route, Redirect } from 'react-router-dom';
 import {authRoutes, publicRoutes} from "../routes";
 
 const AppRouter = () => {
     const isAuth = false
     return (
-        <Routes>
+        <Switch>
             {isAuth && authRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} component={Component} exact/>
             )}
             {publicRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} component={Component} exact/>
             )}
-        </Routes>
+        </Switch>
     );
 };
 
